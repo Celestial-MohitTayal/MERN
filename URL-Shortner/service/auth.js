1dc4b188c48610e23740dd8438fa20c933ba66a5
Celestial-MohitTayal <mohit.tayal@celestialsys.com>
2024-12-13 18:34:48 +0530

    JWT Authentication Added

--- URL-Shortner/service/auth.js
+++ URL-Shortner/service/auth.js
@@ -1,11 +1,27 @@
-const sessionIdtoUserMap = new Map();
+// const sessionIdtoUserMap = new Map();
 
-function setUser(id, user) {
-  sessionIdtoUserMap.set(id, user);
+const jwt = require("jsonwebtoken");
+const secret = "Mohit$2812@";
+
+function setUser(user) {
+  // sessionIdtoUserMap.set(id, user);
+  return jwt.sign(
+    {
+      _id: user._id,
+      email: user.email,
+    },
+    secret
+  ); // user object is working as payload here
 }
 
-function getUser(id) {
-  return sessionIdtoUserMap.get(id);
+function getUser(token) {
+  // return sessionIdtoUserMap.get(id);
+  if (!token) return null;
+  try {
+    return jwt.verify(token, secret);
+  } catch (error) {
+    return null;
+  }
 }
 
 module.exports = { setUser, getUser };
